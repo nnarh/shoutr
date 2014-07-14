96a4098ce25c76afa403e560f41205ef927b06de
Nicholas Narh <nicknarh@gmail.com>
2014-07-14 11:21:24 -0400

    Sign up and in with usernae
    * Added username field to users#new
    * Added username to permitted params for UsersController
    * Swapped email field to user on sessions#new
    * Swapped username for email in permitted params for SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:username, :password)
+    params.require(:session).permit(:username,:email, :password)
   end
 end
 
